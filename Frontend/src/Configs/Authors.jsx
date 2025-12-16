7412f427401ad93de2370107fec01088e1db2e7b
BayRex <akkdladruzej36@gmail.com>
2025-12-16 23:47:48 +0500

    Add files via upload

---
+++ Frontend/src/Configs/Authors.jsx
@@ -0,0 +1,38 @@
+const Authors = [
+    {
+        name: 'BayRex',
+        username: 'bayrex',
+        avatar: 'X.jpg',
+        bio: 'Основатель, главный разработчик и дизайнер проекта (клиентская часть, серверная часть).',
+        social: {
+            element: 'https://elemsocial.com/e/харoми',
+            github: 'https://github.com/xaromie',
+            tg: 'https://t.me/XaromieChannel'
+        }
+    },
+    {
+        name: 'moretti',
+        username: 'moretti',
+        avatar: 'MO.jpg',
+        bio: 'Помощник Xaromie, разработчик проекта (клиентская часть).',
+        social: {
+            element: 'https://elemsocial.com/e/moretti',
+            github: 'https://github.com/morettire',
+            tg: 'https://t.me/elemmoretti'
+        }
+    },
+    {
+        name: 'TRANSCRYPTOR',
+        username: 'TRANSCRYPTOR',
+        avatar: 'T.jpg',
+        bio: 'Помощник Xaromie, разработчик проекта (клиентская часть).',
+        social: {
+            element: 'https://elemsocial.com/e/TRANSCRYPTOR',
+            github: 'https://github.com/TRANSCRYPTOR-progr',
+            tg: 'https://t.me/TRANSCRYPTOR'
+        }
+    }
+
+];
+
+export default Authors;
