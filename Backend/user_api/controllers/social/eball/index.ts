f600aa6919b4e19c36e47b0cc8d85094babf8ee2
BayRex <akkdladruzej36@gmail.com>
2025-12-20 18:38:28 +0500

    Create index.ts

---
+++ Backend/user_api/controllers/social/eball/index.ts
@@ -0,0 +1,11 @@
+import load from './hall/load.js';
+import load_history from './load_history.js';
+import send from './send.js';
+
+export default {
+    hall: {
+        load
+    },
+    send,
+    load_history
+};
