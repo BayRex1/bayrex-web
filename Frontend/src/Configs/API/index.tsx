f87b572bfb439ff71b02de63bb15e16f990bb378
BayRex <akkdladruzej36@gmail.com>
2025-12-22 04:14:01 +0500

    Update index.tsx

--- Frontend/src/Configs/API/index.tsx
+++ Frontend/src/Configs/API/index.tsx
@@ -1,12 +1,12 @@
-import Auth from './Code/Auth';
-import ChangeAccount from './Code/ChangeAccount';
-import Profile from './Code/Profile';
-import Posts from './Code/Posts';
-import Channels from './Code/Channels';
-import Messages from './Code/Messages';
-import Comments from './Code/Comments';
-import CreatePosts from './Code/CreatePosts';
-import Gifts from './Code/Gifts';
+import Auth from '/src/Configs/API/Code/Auth';
+import ChangeAccount from '/src/Configs/API/Code/ChangeAccount';
+import Profile from '/src/Configs/API/Code/Profile';
+import Posts from '/src/Configs/API/Code/Posts';
+import Channels from '/src/Configs/API/Code/Channels';
+import Messages from '/src/Configs/API/Code/Messages';
+import Comments from '/src/Configs/API/Code/Comments';
+import CreatePosts from '/src/Configs/API/Code/CreatePosts';
+import Gifts from '/src/Configs/API/Code/Gifts';
 
 export default {
     Auth: Auth,
