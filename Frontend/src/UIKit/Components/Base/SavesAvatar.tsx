3af640a76bdda8c14f8c61ef8f87b4523a0b61eb
BayRex <akkdladruzej36@gmail.com>
2025-12-21 16:30:41 +0500

    Create SavesAvatar.tsx

---
+++ Frontend/src/UIKit/Components/Base/SavesAvatar.tsx
@@ -0,0 +1,9 @@
+import { I_SAVE } from '../../../System/UI/IconPack';
+
+export const SavesAvatar = () => (
+  <div className='Avatar SavesAvatar'>
+    <I_SAVE />
+  </div>
+);
+
+export default SavesAvatar;
